0289bf92135c4fc5dfb29807150df78fec201dec
agfoghlaim <marieohara.gti@gmail.com>
2021-02-18 19:43:19 +0000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: "Galway Rain",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+  ],
+};
