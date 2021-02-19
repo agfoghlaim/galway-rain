ed87544f2a2d91460ca9d119aa49c8608096ea61
agfoghlaim <marieohara.gti@gmail.com>
2021-02-19 12:41:30 +0000

    Add more siteMetadata so plugin-sitemap is happy.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,13 @@
 module.exports = {
   siteMetadata: {
-    title: "Galway Rain",
-    description: "How often does it rain, really?"
+    title: 'Galway Rain',
+    description: 'How often does it rain, really?',
+    author: 'moh',
+    siteUrl: 'https://galway-rain.netlify.app/',
   },
   plugins: [
-    "gatsby-plugin-sass",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
+    'gatsby-plugin-sass',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
   ],
 };
