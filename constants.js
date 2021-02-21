45f153ffbc1f5626942a7fac935c69a197c59165
agfoghlaim <marieohara.gti@gmail.com>
2021-02-21 21:16:47 +0000

    Removed ref to window (forgot about ssr).

--- constants.js
+++ constants.js
@@ -5,8 +5,10 @@
  */
 
 // Quick and filty solution to give people on mobile a chance.
-export const NUM_DAYS_IN_ROW = window.innerWidth > 500 ? 10 : 5;
-export const NUM_DAYS_IN_GAME = window.innerWidth > 500 ? 100 : 100;
+// export const NUM_DAYS_IN_ROW = window.innerWidth > 500 ? 10 : 5;
+// export const NUM_DAYS_IN_GAME = window.innerWidth > 500 ? 100 : 100;
+export const NUM_DAYS_IN_ROW = 10;
+export const NUM_DAYS_IN_GAME = 100;
 
 export const DIRECTIONS = [
   'west',
